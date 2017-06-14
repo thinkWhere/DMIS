9cafd7513a7454848bcc1317aac589e80d636f5e
LindaAlblas <linda.alblas@thinkwhere.com>
2017-06-14 17:09:14 +0100

    Angular 4 + OpenLayers

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import * as ol from 'openlayers';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DMIS';
+
+  ngOnInit() {
+    var map = new ol.Map({
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      target: 'map',
+      view: new ol.View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+}
