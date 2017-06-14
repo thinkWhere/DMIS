3367a2c9b8f25cf2df9fa193734ff1cefcefa9a0
Colin Wright <colin.wright@thinkwhere.com>
2017-06-14 17:52:57 +0100

    Add shippable deploy

--- bin/linux/deploy.sh
+++ bin/linux/deploy.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-echo Running DMIS App Deploy, current branch is $BRANCH
-echo Target Branch is $BASE_BRANCH
-echo Head Branch is $HEAD_BRANCH
-echo Is Pull Request is $IS_PULL_REQUEST
+
+echo Running DMIS Deploy, current branch is $BRANCH
 
 # We don't want to deploy Pull Requests only builds on develop and master
 if [ $IS_PULL_REQUEST == true ]
@@ -15,26 +13,15 @@ fi
 # Set Version Number
 VERSION=v.0.0.$BUILD_NUMBER-$BRANCH
 
-# Only deploy to Staging if we're on develop and testing the Staging Environment
+# Only deploy to Staging if we're on develop
 if [ $BRANCH == "develop" ]
     then
-        pip install --upgrade pip
+        # Install AWS requirements
         pip install -r requirements.txt
-
-        # TODO - establish way of securing credentials
-        #export AWS_CREDENTIAL_FILE=./.ebextensions/eb.credentials
-
-        # Set appropriate environment var
-        mv ./.ebextensions/environment.staging ./.ebextensions/environment.config
-        printf '1\nn\n' | eb init dmis --region eu-west-1
-
-        # Uncomment if debug needed
-        # tail ./.elasticbeanstalk/config.yml
-        # tail ./.ebextensions/environment.config
+        printf 'n\n' | eb init dmis --region eu-west-1
+        eb use dmis-staging
 
         # Deploy develop builds to Staging environment
         echo Deploying $VERSION to dmis-staging
-        eb use dmis-staging
         eb deploy -l $VERSION
-
 fi
