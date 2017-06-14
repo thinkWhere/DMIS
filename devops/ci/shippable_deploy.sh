533079ca4013aa2c99e607df40ac2256b3eec485
Colin Wright <colin.wright@thinkwhere.com>
2017-06-14 17:49:11 +0100

    Add shippable deploy

---
+++ devops/ci/shippable_deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+echo Running DMIS Deploy, current branch is $BRANCH
+
+# We don't want to deploy Pull Requests only builds on develop and master
+if [ $IS_PULL_REQUEST == true ]
+    then
+        echo Not Deploying Build $BUILD_NUMBER - Branch is $BRANCH, Is Pull Request is $IS_PULL_REQUEST
+        return
+fi
+
+# Set Version Number
+VERSION=v.0.0.$BUILD_NUMBER-$BRANCH
+
+# Only deploy to Staging if we're on develop
+if [ $BRANCH == "develop" ]
+    then
+        # Install AWS requirements
+        pip install -r requirements.txt
+        printf 'n\n' | eb init dmis --region eu-west-1
+        eb use dmis-staging
+
+        # Deploy develop builds to Staging environment
+        echo Deploying $VERSION to dmis-staging
+        eb deploy -l $VERSION
+fi
