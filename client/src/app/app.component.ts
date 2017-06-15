132ec371c5aceac1c6e4a6f12f3f7fd5d9949da7
LindaAlblas <linda.alblas@thinkwhere.com>
2017-06-15 16:02:09 +0100

    swap component css for scss

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,7 +4,7 @@ import * as ol from 'openlayers';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'DMIS';
