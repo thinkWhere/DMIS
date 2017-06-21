b6be8b228b961a9fb638a7cb710c5a9d48246bfe
LindaAlblas <linda.alblas@thinkwhere.com>
2017-06-21 17:00:30 +0100

    tidy up

--- client/src/app/shared/authentication.service.ts
+++ client/src/app/shared/authentication.service.ts
@@ -72,14 +72,6 @@ export class AuthenticationService {
    * @returns {ErrorObservable}
      */
   private handleError (error: Response | any) {
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
-    }
-    return Observable.throw(errMsg);
+    return Observable.throw('unspecified error');
   }
 }
