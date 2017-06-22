23bcefb5eb6fbe77591385194d4e6f1891e6a2c3
Iain Hunter <iain.hunter@thinkwhere.com>
2017-06-22 11:08:13 +0100

    Bugfix - Add template back to Blueprint route

--- server/web/__init__.py
+++ server/web/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
 swagger = Blueprint('swagger', __name__, static_folder='static/swagger-ui', url_prefix='/api-docs')
-main = Blueprint('main', __name__, static_folder='static/dist/en')
+main = Blueprint('main', __name__, static_folder='static/dist/en', template_folder='templates')
 
 from . import controller
