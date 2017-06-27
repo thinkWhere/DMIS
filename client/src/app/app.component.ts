e0cb7734e16d72af8470c1f0d4df3d73f6f655c0
Linda Alblas <LindaAlblas@users.noreply.github.com>
2017-06-27 10:51:47 +0100

    Switch categories (#17)
    * add headers
    
    * tidy up
    
    * fix merge issue

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,39 +1,49 @@
-import { Component } from '@angular/core';
-import { Subscription }   from 'rxjs/Subscription';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {Subscription}   from 'rxjs/Subscription';
 
-import { AuthenticationService } from './shared/authentication.service';
+import {AuthenticationService} from './shared/authentication.service';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  // TODO: create user service
-  user: any;
-  subscription: Subscription;
+    // TODO: create user service
+    user:any;
+    subscription:Subscription;
+    route:string;
 
-  constructor(
-      private authenticationService: AuthenticationService,
-      private router: Router
-  ){}
+    constructor(
+        private authenticationService:AuthenticationService,
+        private router:Router
+    ) {}
 
-  ngOnInit() {
-    this.user = JSON.parse(localStorage.getItem('currentUser'));
-    this.subscription = this.authenticationService.usernameChanged$.subscribe(
-      value => {
+    ngOnInit() {
+        // Checks the route for displaying the categories in the header
+        this.route = this.router.url;
+        this.router.events.subscribe((event) => {
+            this.route = this.router.url;
+        });
+
+        // Checks if the user is logged in
         this.user = JSON.parse(localStorage.getItem('currentUser'));
-      });
-  }
+        this.subscription = this.authenticationService.usernameChanged$.subscribe(
+            value => {
+                this.user = JSON.parse(localStorage.getItem('currentUser'));
+            });
+    }
 
-  /**
-   * Log the user out
-   */
-  logout(){
-    this.authenticationService.logout();
-    this.user = JSON.parse(localStorage.getItem('currentUser'));
-    this.router.navigateByUrl('/');
-  }
+    /**
+     * Log the user out
+     */
+    logout() {
+        this.authenticationService.logout();
+        this.user = JSON.parse(localStorage.getItem('currentUser'));
+        this.router.navigateByUrl('/');
+    }
 }
+
