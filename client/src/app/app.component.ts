695ab7c3d2a7032940229834f34a1803712be9ea
LindaAlblas <linda.alblas@thinkwhere.com>
2017-07-06 15:22:42 +0100

    restrict routing with authGuard

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -30,7 +30,8 @@ export class AppComponent {
         });
 
         // Checks if the user is logged in
-        this.user = JSON.parse(localStorage.getItem('currentUser'));
+        //this.user = JSON.parse(localStorage.getItem('currentUser'));
+        this.user = this.authenticationService.checkLoginStatus();
         this.subscription = this.authenticationService.usernameChanged$.subscribe(
             value => {
                 this.user = JSON.parse(localStorage.getItem('currentUser'));
