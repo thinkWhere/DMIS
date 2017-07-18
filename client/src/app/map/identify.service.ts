15a9c1565ed3c9568fdabc45b76b0fe86d72e99a
LindaAlblas <linda.alblas@thinkwhere.com>
2017-07-18 15:34:22 +0100

    identify pacific disaster centre layer

--- client/src/app/map/identify.service.ts
+++ client/src/app/map/identify.service.ts
@@ -9,11 +9,15 @@ import { AuthenticationService } from './../shared/authentication.service';
 @Injectable()
 export class IdentifyService {
 
+    /** Popup **/
     container: any;
     content: any;
     closer: any;
     overlay: any;
+
+    /** Identify **/
     maxFeatureCount: number = 10;
+    identifyAsyncCalls: any;
 
     constructor(
         private http: Http,
@@ -56,15 +60,24 @@ export class IdentifyService {
 
     /**
      * Add identify event handlers
+     * TODO: review after adding type of layer to DB/API
      * @param map
      * @param source
      */
     addIdentifyEventHandlers(map, source){
         map.on('singleclick', (evt) => {
+            // The identify calls go to different sources depending on the type of layer
+            // So we need to wait for all the calls to finish before displaying a popup
+            this.identifyAsyncCalls = {
+                pdcArcGISRest: false,
+                dmisGetFeatureInfo: false
+            };
             this.content.innerHTML = '';
             this.overlay.setPosition(null);
+            var coordinate = evt.coordinate;
+            // WMS GetFeatureInfo
             var viewResolution = map.getView().getResolution();
-            var url = source.getGetFeatureInfoUrl(
+            var getFeatureInfoUrl = source.getGetFeatureInfoUrl(
                 evt.coordinate, viewResolution, 'EPSG:3857',
                 {
                     'INFO_FORMAT': 'application/json',
@@ -72,54 +85,63 @@ export class IdentifyService {
                     'BUFFER': 10
                 });
             var identifiableLayers = this.layerService.getIdentifiableLayers(map, 'wms');
-            url = this.updateUrlParameter(url, 'QUERY_LAYERS', identifiableLayers.join());
-            url = this.updateUrlParameter(url, 'LAYERS', identifiableLayers.join());
-            if (url && identifiableLayers.length > 0) {
-                var coordinate = evt.coordinate;
+            getFeatureInfoUrl = this.updateUrlParameter(getFeatureInfoUrl, 'QUERY_LAYERS', identifiableLayers.join());
+            getFeatureInfoUrl = this.updateUrlParameter(getFeatureInfoUrl, 'LAYERS', identifiableLayers.join());
+            if (getFeatureInfoUrl && identifiableLayers.length > 0) {
                 var parser = new ol.format.GeoJSON();
-                this.getFeatureInfo(url)
+                this.getFeatureInfo(getFeatureInfoUrl)
                         .subscribe(
                         data => {
                             // Success
+                            this.identifyAsyncCalls.dmisGetFeatureInfo = true;
                             var result = parser.readFeatures(data);
-                            this.populatePopup(result);
-                            this.overlay.setPosition(coordinate);
+                            var content = this.generatePopupContent(result);
+                            this.content.innerHTML += content;
+                            this.checkOtherCallsAndShowPopup(coordinate);
                         },
                         error => {
                             // TODO: handle error?
+                            this.identifyAsyncCalls.dmisGetFeatureInfo = true;
                         }
                     );
             }
-            // TODO: make more generic
+            else {
+                // No call is made so set it to finished
+                this.identifyAsyncCalls.dmisGetFeatureInfo = true;
+            }
+
+            // Use ArcGIS REST identify
             var arcgisLayers = this.layerService.getIdentifiableLayers(map, 'arcgisrest');
-            console.log(evt);
-            console.log(map.getSize());
             var geometry = evt.coordinate[0] + ',' + evt.coordinate[1];
-            var mapExtent = '-20026376.39,20026376.39,-20048966.10,20048966.10';
+            var mapExtent = '-20037700,20037700,-30241100,30241100';
             var imageDisplay = map.getSize()[0] + ',' + map.getSize()[1] + ',' + '72';
-            var point = ol.proj.transform([-120.17797, 37.5923],'EPSG:4326', 'EPSG:3857');
-            console.log(point);
-            url = 'http://arcgis.pdc.org/arcgis/rest/services/global_public/pdc_integrated_active_hazards/MapServer/identify?' +
+            var arcUrl = 'http://arcgis.pdc.org/arcgis/rest/services/global_public/pdc_integrated_active_hazards/MapServer/identify?' +
                 'geometry=' + geometry +
                 '&geometryType=esriGeometryPoint' +
                 '&layers=all&tolerance=10' +
                 '&mapExtent=' + mapExtent +
                 '&imageDisplay=' + imageDisplay +
                 '&returnGeometry=false&f=json';
-            if (url){
-                 this.identifyArcGISRest(url)
+            if (arcUrl && arcgisLayers.length > 0){
+                 this.identifyArcGISRest(arcUrl)
                         .subscribe(
                         data => {
                             // Success
-                           console.log(data);
+                            this.identifyAsyncCalls.pdcArcGISRest = true;
+                            var content = this.generateArcPopupContent(data.results);
+                            this.content.innerHTML += content;
+                            this.checkOtherCallsAndShowPopup(coordinate);
                         },
                         error => {
-                            console.log(error);
                             // TODO: handle error?
+                             this.identifyAsyncCalls.pdcArcGISRest = true;
                         }
                     );
             }
-
+            else {
+                // No call is made so set it to finished
+                this.identifyAsyncCalls.pdcArcGISRest = true;
+            }
         });
     }
 
@@ -132,6 +154,22 @@ export class IdentifyService {
          return this.http.get(url)
                 .map(response => response.json())
                 .catch(this.handleError);
+    }
+
+    /**
+     * Show a popup wwhen all identify calls have finished
+     * @param coordinate
+     */
+    private checkOtherCallsAndShowPopup(coordinate){
+        for (var call in this.identifyAsyncCalls){
+            if (!this.identifyAsyncCalls[call]){
+                return;
+            }
+        }
+        if (this.content.innerHTML === ''){
+            this.content.innerHTML = 'No information available.';
+        }
+        this.overlay.setPosition(coordinate);
     }
 
     /**
@@ -152,11 +190,12 @@ export class IdentifyService {
     }
 
     /**
-     * Populates the popup with the identify results
+     * Generates the popup content with the identify results
      * TODO: create template for popup?
      * @param result
      */
-    private populatePopup(result) {
+    private generatePopupContent(result) {
+        var htmlContent = '';
         if (result.length) {
             var tableContent = '';
             for (var i = 0, ii = result.length; i < ii; ++i) {
@@ -183,10 +222,45 @@ export class IdentifyService {
                 tableContent += '</tbody>';
                 tableContent += '</table>';
             }
-            this.content.innerHTML = tableContent;
-        } else {
-            this.content.innerHTML = 'No information available.';
+            htmlContent = tableContent;
         }
+        return htmlContent;
+    }
+
+    /**
+     * Generates the popup content for ArcGIS REST Identify results
+     * @param result
+     * @returns {string}
+     */
+    private generateArcPopupContent(result) {
+        var htmlContent = '';
+        if (result.length) {
+            var tableContent = '';
+            for (var i = 0; i < result.length; i++) {
+                tableContent += '<h4>' + result[i].layerName + '</h4>';
+                tableContent += '<table class="table table-condensed">';
+                tableContent += '<tbody>';
+                tableContent += '<thead><tr><th>Property</th><th>Value</th></tr></thead>';
+                var attributes = result[i].attributes;
+                // exclude geometry property
+                for (var property in attributes) {
+                        tableContent += '<tr>';
+                        // Property key
+                        tableContent += '<th scope="row">';
+                        tableContent += property;
+                        tableContent += '</td>';
+                        // Property value
+                        tableContent += '<td>';
+                        tableContent += attributes[property];
+                        tableContent += '</td>';
+                        tableContent += '</tr>';
+                }
+                tableContent += '</tbody>';
+                tableContent += '</table>';
+            }
+            htmlContent = tableContent;
+        }
+        return htmlContent;
     }
 
     /**
