ac4c5e561e9114e1d08cbbe4ace3d7c222a3f3a2
Linda Alblas <LindaAlblas@users.noreply.github.com>
2017-07-19 13:26:37 +0100

    Esri rest layer (#45)
    * work in progress
    
    * identify pacific disaster centre layer
    
    * remove hardcoded identify url

--- client/src/app/map/layer.service.ts
+++ client/src/app/map/layer.service.ts
@@ -26,24 +26,39 @@ export class LayerService {
     let options = new RequestOptions({ headers: headers});
 
     // TODO: make category a parameter
-    return this.http.get(environment.apiEndpoint + '/layer/toc/preparedness', options)
+    return this.http.get(environment.apiEndpoint + '/layer/toc/UNKNOWN', options)
         .map(response => response.json())
         .catch(this.handleError);
   }
 
   /**
    * Get identifiable layers
+   * TODO: refactor when types are implemented
    * @param map
+   * @param type
    * @returns {Array}
      */
-  getIdentifiableLayers(map) {
+  getIdentifiableLayers(map, type) {
     let identifiableLayers = [];
     let layers = map.getLayers().getArray();
     // find the layer
     for (var i = 0; i < layers.length; i++) {
       // Only identify layers visible on the map
-      if (layers[i].getVisible() && layers[i].getProperties().layerName){
-         identifiableLayers.push(layers[i].getProperties().layerName)
+      if (type === 'wms'){
+        if (layers[i].getVisible() && layers[i].getProperties().layerName){
+          // TODO: filter identifiable layers per type
+          if (layers[i].getProperties().layerName !== 'pdc_integrated_active_hazards'){
+            identifiableLayers.push(layers[i].getProperties().layerName)
+          }
+        }
+      }
+      if (type === 'arcgisrest'){
+        if (layers[i].getVisible() && layers[i].getProperties().layerName){
+          // TODO: filter identifiable layers per type
+          if (layers[i].getProperties().layerName === 'pdc_integrated_active_hazards'){
+            identifiableLayers.push(layers[i])
+          }
+        }
       }
     }
     return identifiableLayers;
