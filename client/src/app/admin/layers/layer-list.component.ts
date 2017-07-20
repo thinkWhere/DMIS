4fc295cd87d5cb92f501dd0c76f89fcb10a04af0
LindaAlblas <linda.alblas@thinkwhere.com>
2017-07-20 14:17:56 +0100

    tidy up

--- client/src/app/admin/layers/layer-list.component.ts
+++ client/src/app/admin/layers/layer-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { OrderByPipe } from 'angular-pipes/src/array/order-by.pipe';
 
 import { LayerService } from './layer.service';
 
