9a5d163508c1c4ad9c9d152d1f564d80ce48b2dc
LindaAlblas <linda.alblas@thinkwhere.com>
2017-07-25 16:59:18 +0100

    language dependant weather map

--- client/src/app/weather/weather.component.ts
+++ client/src/app/weather/weather.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  languageCode: string = 'en';
+
+  constructor(
+      private locationStrategy: LocationStrategy
+  ) { }
 
   ngOnInit() {
+     // Checks which language toggle to show
+        var baseHref = this.locationStrategy.getBaseHref();
+        // If base href is not / or English, then set the language code
+        if (baseHref !== '/'){
+            // Remove leading '/'
+            this.languageCode = baseHref.substring(1);
+        }
   }
-
 }
