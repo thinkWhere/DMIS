7b7d33ba366736c41b09d4879aee1fec44243276
LindaAlblas <linda.alblas@thinkwhere.com>
2017-08-15 11:48:56 +0100

    tidy up CRS + add test + comments

--- server/services/data_ingest/data_ingest_service.py
+++ server/services/data_ingest/data_ingest_service.py
@@ -18,13 +18,18 @@ class DataIngestService:
         data_source = data_source.lower()
         dmis_data = DMISData()
 
+        dynamic_assessment_data_sources = ['ktm_pcdm_affected_healthcenter', 'ktm_pcdm_affected_road',
+                                           'ktm_pcdm_affected_school', 'ktm_pcdm_affected_wells',
+                                           'ktm_pcdm_at_risk_commune', 'ktm_pcdm_at_risk_village',
+                                           'ktm_pcdm_data_daily_actual']
+
         if data_source == 'river-gauge':
             # TODO may want to save to a separate river gauge table in future
             dmis_data.save_json_data(data_source, raw_request.get_json())
         elif data_source == 'hub':
             # TODO more parsing will be needed to understand exactly what type of data is here
             dmis_data.save_json_data(data_source, raw_request.get_json())
-        elif data_source == 'test':
+        elif data_source in dynamic_assessment_data_sources:
             geojson = DataIngestService._process_arcgis_json(raw_request.get_json())
             dmis_data.save_json_data(data_source, geojson)
         else:
@@ -32,6 +37,11 @@ class DataIngestService:
 
     @staticmethod
     def _process_arcgis_json(arcgis_json):
+        """
+        Converts ArcGIS json into GeoJSON
+        :param arcgis_json: ArcGIS json object
+        :return: FeatureCollection (GeoJSON)
+        """
         features = []
         source_features = arcgis_json.get('features', [])
         for source_feature in source_features:
@@ -43,12 +53,20 @@ class DataIngestService:
             features.append(new_feature)
         # Add CRS
         epsg_code = arcgis_json.get('spatialReference', {}).get('wkid', 3857)
-        crs = {
+        dataset = FeatureCollection(features, crs=DataIngestService._get_geojson_crs(epsg_code))
+        return dataset
+
+    @staticmethod
+    def _get_geojson_crs(epsg_code):
+        """
+        Gets the GeoJSON crs identifier for an EPSG code
+        :param epsg_code:
+        :return:
+        """
+        return {
             "type": "name",
             "properties": {
                 "name": f"EPSG:{epsg_code}"
             }
         }
-        dataset = FeatureCollection(features, crs=crs)
-        return dataset
 
