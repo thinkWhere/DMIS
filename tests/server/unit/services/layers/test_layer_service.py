0810a9d40530b704ef289915b997b2878c0ddf75
Iain Hunter <iain.hunter@thinkwhere.com>
2017-08-28 15:12:33 +0100

    Remove layer cols (#91)
    * Remove old layer cols
    
    * Update tests

--- tests/server/unit/services/layers/test_layer_service.py
+++ tests/server/unit/services/layers/test_layer_service.py
@@ -10,12 +10,8 @@ class TestLayerService(unittest.TestCase):
     def test_unknown_layer_type_raises_error(self):
         layer_details = LayerDetailsDTO()
         layer_details.layer_name = 'name'
-        layer_details.layer_title = 'title'
-        layer_details.layer_description = 'description'
         layer_details.map_category = MapCategory.PREPAREDNESS.name
-        layer_details.layer_group = 'group'
         layer_details.layer_source = 'source'
-        layer_details.layer_copyright = 'copyright'
         layer_details.layer_type = 'superman'
 
         with self.assertRaises(DataError):
