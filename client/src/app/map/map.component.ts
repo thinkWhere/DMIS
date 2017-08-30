53c41f6e0f7f47635a7c2643567565a6756c3513
LindaAlblas <linda.alblas@thinkwhere.com>
2017-08-30 17:09:39 +0100

    start of legend images

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -252,33 +252,8 @@ export class MapComponent implements OnInit {
      * @param layer
      */
     private setGeoJSONLegend(layer) {
-        // Create a canvas element
-        var canvas : any = document.createElement("canvas");
-
-        // Add images to it
-        var vectorContext = ol.render.toContext(canvas.getContext('2d'), {size: [80, 25]});
-
-        var fill = new ol.style.Fill({color: 'blue'});
-        var stroke = new ol.style.Stroke({color: 'black'});
-        var style = new ol.style.Style({
-            fill: fill,
-            stroke: stroke,
-            image: new ol.style.Circle({
-                radius: 5,
-                fill: fill,
-                stroke: stroke
-            })
-        });
-        vectorContext.setStyle(style);
-        vectorContext.drawGeometry(new ol.geom.Point([10, 10]));
-
-        var ctx = canvas.getContext("2d");
-        ctx.font = "10px Arial";
-        ctx.fillText("Test legend",30,15);
-
-        // Convert it into an image that can be used as a legend
-        var dataURL = canvas.toDataURL();
-        layer.layerLegend = dataURL;
+        var legendImage = this.styleService.getLegendImage(layer.layerStyle);
+        layer.layerLegend = legendImage;
     }
 
     /**
@@ -406,12 +381,13 @@ export class MapComponent implements OnInit {
             });
             var features = layer.getSource().getFeatures();
             for (var i = 0; i < features.length; i++){
-                features[i].setProperties({
-                    layerStyle: layerData.layerStyle
-                })
+                //features[i].setProperties({
+                //    layerStyle: layerData.layerStyle
+                //});
+                features[i].setStyle(this.styleService.getStyle(features[i], layerData.layerStyle));
             }
 
-            layer.setStyle(this.styleService.getStyle)
+            //layer.setStyle(this.styleService.getStyle)
         }
         layer.setVisible(false);
         layer.setProperties({
--- client/src/app/map/style.service.ts
+++ client/src/app/map/style.service.ts
@@ -9,14 +9,15 @@ export class StyleService {
 
     /**
      * Get style
+     * TODO: validate layerStyle?
      * @param feature
+     * @param layerStyle
      * @returns {any}
      */
-    getStyle(feature) {
+    getStyle(feature, layerStyle) {
         var style: any;
-        var layerStyle = feature.getProperties().layerStyle;
-        if (!layerStyle){
-            style = getDefaultStyle();
+        if (!layerStyle) {
+            style = this.getDefaultStyle();
             return style;
         }
         for (var i = 0; i < layerStyle.rules.length; i++) {
@@ -28,38 +29,123 @@ export class StyleService {
                 var comparisonType = filter.comparisonType;
                 if (comparisonType === 'BETWEEN') {
                     if (feature.get(propertyName) >= filter.min && feature.get(propertyName) < filter.max) {
-                        style = getOLStyle(ruleStyle);
+                        style = this.getOLStyle(ruleStyle);
                         return style;
                     }
                 }
                 if (comparisonType === 'GREATER_THAN') {
                     if (feature.get(propertyName) > filter.min) {
-                        style = getOLStyle(ruleStyle);
+                        style = this.getOLStyle(ruleStyle);
                         return style;
                     }
                 }
                 if (comparisonType === 'EQUALS') {
                     if (feature.get(propertyName) == filter.value) {
-                        style = getOLStyle(ruleStyle);
+                        style = this.getOLStyle(ruleStyle);
                         return style;
                     }
                 }
             }
             else {
                 // No filter
-                style = getOLStyle(ruleStyle);
+                style = this.getOLStyle(ruleStyle);
                 return style;
             }
         }
-        /**
-         * Get the OL style based on the rule style
-         * This is a nested function to allow access to it from the style function
-         * @param ruleStyle
-         * @returns {any}
-         */
-        function getOLStyle(ruleStyle) {
+    }
+
+    /**
+     * Get the OL style based on the rule style
+     * @param ruleStyle
+     * @returns {any}
+     */
+    getOLStyle(ruleStyle) {
+        if (ruleStyle.text) {
+            var style: any = new ol.style.Style({
+                text: new ol.style.Text({
+                    text: ruleStyle.text.text,
+                    font: ruleStyle.text.font,
+                    textBaseline: 'Bottom',
+                    fill: new ol.style.Fill({
+                        color: ruleStyle.text.fill.colour,
+                    }),
+                    stroke: new ol.style.Stroke({
+                        color: ruleStyle.text.stroke.colour,
+                        width: ruleStyle.text.stroke.width
+                    })
+                })
+            });
+            return style;
+        }
+        var style: any = new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: ruleStyle.stroke.colour,
+                width: ruleStyle.stroke.width
+            }),
+            fill: new ol.style.Fill({
+                color: ruleStyle.fill.colour
+            })
+        });
+        return style;
+    }
+
+    /**
+     * Style function: returns a default style
+     * @returns {ol.style.Style}
+     */
+    getDefaultStyle() {
+        // Default style
+        var style = new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: 4,
+                stroke: new ol.style.Stroke({
+                    color: 'red',
+                    width: 2
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(255, 255, 255, 0.4)'
+                })
+            }),
+            stroke: new ol.style.Stroke({
+                color: 'red',
+                width: 1
+            }),
+            fill: new ol.style.Fill({
+                color: 'rgba(255, 255, 255, 0.4)'
+            })
+        });
+        return style;
+    }
+
+    /**
+     * Get legend image
+     * TODO: work in progress
+     * @param layerStyle
+     * @returns {string}
+     */
+    getLegendImage(layerStyle) {
+        if (!layerStyle) {
+            // TODO: return default style legend image
+            return;
+        }
+        // Create a canvas element
+        var canvas: any = document.createElement("canvas");
+
+        // Create the context using OL
+        var baseHeight = 10;
+        var heightPerCategory = 20;
+        var height = baseHeight + heightPerCategory * layerStyle.rules.length;
+        var width = 200;
+        var vectorContext = ol.render.toContext(canvas.getContext('2d'), {size: [width, height]});
+
+        for (var i = 0; i < layerStyle.rules.length; i++) {
+            var ruleStyle = layerStyle.rules[i].style;
+            var label = layerStyle.rules[i].label;
+            // Get style - very similar to getDefaultStyle which is a nested function
+            // TODO: review
+            var style: any;
             if (ruleStyle.text) {
-                var style: any = new ol.style.Style({
+                style = new ol.style.Style({
                     text: new ol.style.Text({
                         text: ruleStyle.text.text,
                         font: ruleStyle.text.font,
@@ -73,45 +159,33 @@ export class StyleService {
                         })
                     })
                 });
-                return style;
             }
-            var style: any = new ol.style.Style({
-                stroke: new ol.style.Stroke({
-                    color: ruleStyle.stroke.colour,
-                    width: ruleStyle.stroke.width
-                }),
-                fill: new ol.style.Fill({
-                    color: ruleStyle.fill.colour
-                })
-            });
-            return style;
-        }
-        /**
-         * Style function: returns a default style
-         * @returns {ol.style.Style}
-         */
-        function getDefaultStyle() {
-            // Default style
-            var style = new ol.style.Style({
-                image: new ol.style.Circle({
-                    radius: 4,
-                    stroke: new ol.style.Stroke({
-                        color: 'red',
-                        width: 2
-                    }),
-                    fill: new ol.style.Fill({
-                        color: 'rgba(255, 255, 255, 0.4)'
+            else {
+                // Add square
+                style = new ol.style.Style({
+                    image: new ol.style.RegularShape({
+                        radius: 10,
+                        points: 4,
+                        angle: Math.PI / 4,
+                        stroke: new ol.style.Stroke({
+                            color: ruleStyle.stroke.colour,
+                            width: ruleStyle.stroke.width
+                        }),
+                        fill: new ol.style.Fill({
+                            color: ruleStyle.fill.colour
+                        })
                     })
-                }),
-                stroke: new ol.style.Stroke({
-                    color: 'red',
-                    width: 1
-                }),
-                fill: new ol.style.Fill({
-                    color: 'rgba(255, 255, 255, 0.4)'
-                })
-            });
-            return style;
+                });
+            }
+            vectorContext.setStyle(style);
+            vectorContext.drawGeometry(new ol.geom.Point([10, (10 + i * heightPerCategory)]));
+
+            var ctx = canvas.getContext("2d");
+            ctx.font = "10px Arial";
+            ctx.fillText(label, 30, (15 + i * heightPerCategory));
         }
+        // Convert it into an image that can be used as a legend
+        var dataURL = canvas.toDataURL();
+        return dataURL;
     }
 }
