b38452b3a0207c56c2d8d1f6861b6e920a10c03b
LindaAlblas <linda.alblas@thinkwhere.com>
2017-09-04 08:58:25 +0100

    fix style service test

--- client/src/app/map/style.service.spec.ts
+++ client/src/app/map/style.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 
 import { StyleService } from './style.service';
 
 describe('StyleService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpModule],
       providers: [StyleService]
     });
   });
