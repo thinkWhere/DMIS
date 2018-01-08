677af9b0c7e6a03ffc781369462925bff93ea970
Iain Hunter <iain.hunter@thinkwhere.com>
2018-01-08 13:42:41 +0000

    Test deploy

--- devops/ci/deploy.sh
+++ devops/ci/deploy.sh
@@ -19,7 +19,8 @@ if [ $BRANCH == "develop" ]
         pip install --upgrade pip
         
         # Install AWS requirements
-        LC_ALL=C pip install -r requirements.aws-deploy.txt
+        export LC_ALL=C
+        pip install -r requirements.aws-deploy.txt --no-cache-dir
         printf 'n\n' | eb init dmis --region eu-west-1
         eb use dmis-staging
 
